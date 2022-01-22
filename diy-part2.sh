db82f2f9a9b8478d2e8cffe41e5384a0a932ebfb
Mouse3701 <97789607+Mouse3701@users.noreply.github.com>
2022-01-22 16:57:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/10.10.10.5/g' package/base-files/files/bin/config_generate
 
 # argon
-sed -i s/luci-theme-bootstrap/luci-theme-argon/g feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Mouse3701
-sed -i s/OpenWrt /Mouse3701 compiled in $(TZ=UTC-8 date +%Y.%m.%d) @ OpenWrt /g
+sed -i 's/OpenWrt /Mouse3701 compiled in $(TZ=UTC-8 date +%Y.%m.%d) @ OpenWrt /g'
