0c3eb861b79b64309a0af92c0901f0cd3e5a1d48
Mouse3701 <97789607+Mouse3701@users.noreply.github.com>
2022-01-22 11:08:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,11 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.5/g' package/base-files/files/bin/config_generate
+
+# argon
+sed -i s/luci-theme-bootstrap/luci-theme-argon/g feeds/luci/collections/luci/Makefile
+# 00000
+ZZZ=package/lean/default-settings/files/zzz-default-settings
+sed -i /CYXluq4wUazHjmCDBCqXF/d $ZZZ
+# Mouse3701
+sed -i s/OpenWrt /Mouse3701 compiled in $(TZ=UTC-8 date +%Y.%m.%d) @ OpenWrt /g $ZZZ
